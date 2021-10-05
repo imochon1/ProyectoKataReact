9edc54c087805964a9ae863899881349fdc9df2f
Isaac_M <isaacmochon@gmail.com>
2021-10-05 00:52:36 -0500

    se trabajo css de login en destructured

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,94 +6,110 @@ import {
   Box,
   OutlinedInput,
   Button,
+  Card,
+  CardContent,
 } from "@mui/material/";
 import SendIcon from "@mui/icons-material/Send";
 import AlternateEmailOutlinedIcon from "@mui/icons-material/AlternateEmailOutlined";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import "./Login.css";
+//import { validateEmail } from "../utils/utilities";
 function Login() {
-  // eslint-disable-next-line no-unused-vars
-  // eslint-disable-next-line no-unused-vars
-
   const [usuario, setUsuario] = useState({});
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const handleEmail = ({ target }) => {
-    setEmail(target.value);
-    console.log("handlechange", email);
-  };
+  const [showError, setShowError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handlePassword = ({ target }) => {
-    setPassword(target.value);
-    console.log("handlechange", password);
+  const inputHandler = ({ target: { name, value } }) => {
+    setUsuario({ ...usuario, [name]: value });
+    console.log("usuario", usuario);
   };
 
   const sendLoginInfo = () => {
-    console.log("email:", email, "password:", password);
+    if (usuario.email === undefined || usuario.email.length === 0) {
+      setErrorMessage("Campo de Email Vacio");
+      alert(errorMessage);
+      setShowError(true);
+      return;
+    }
+    if (usuario.password === undefined || usuario.password.length === 0) {
+      setErrorMessage("Password Requerido");
+      alert(errorMessage);
+      setShowError(true);
+      return;
+    }
+
+    setUsuario(usuario);
+    console.log(usuario.email, usuario.password);
   };
 
   return (
-    <>
-      <Box
-        className="loginWrapper"
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1 },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <FormControl>
-          <InputLabel htmlFor="component-outlined">
-            <AlternateEmailOutlinedIcon />
-          </InputLabel>
-          <OutlinedInput
-            id="component-outlined"
-            value={email}
-            onChange={handleEmail}
-            label="Name"
-          />
-        </FormControl>
-      </Box>
-      <Box
-        className="loginWrapper"
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1 },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <FormControl>
-          <InputLabel htmlFor="component-outlined">
-            <LockOutlinedIcon />
-          </InputLabel>
-          <OutlinedInput
-            id="component-outlined"
-            value={password}
-            onChange={handlePassword}
-            label="Name"
-          />
-        </FormControl>
-      </Box>
-      <Box
-        className="loginWrapper"
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1 },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <Button
-          variant="contained"
-          endIcon={<SendIcon />}
-          onClick={sendLoginInfo}
-        >
-          Send
-        </Button>
-      </Box>
-    </>
+    <div className="card-container">
+      <Card className="card" elevation={4}>
+        <CardContent>
+          <h3>Bienvenid@</h3>
+          <Box
+            className="loginWrapper"
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1 },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <FormControl style={{ marginTop: 35 }}>
+              <InputLabel htmlFor="component-outlined">
+                <AlternateEmailOutlinedIcon />
+              </InputLabel>
+              <OutlinedInput
+                id="component-outlined"
+                name="email"
+                onChange={inputHandler}
+                label="Email"
+              />
+            </FormControl>
+          </Box>
+          <Box
+            className="loginWrapper"
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1 },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <FormControl style={{ marginTop: 35 }}>
+              <InputLabel htmlFor="component-outlined">
+                <LockOutlinedIcon />
+              </InputLabel>
+              <OutlinedInput
+                id="component-outlined"
+                name="password"
+                onChange={inputHandler}
+                label="passwird"
+              />
+            </FormControl>
+          </Box>
+          <Box
+            className="loginWrapper"
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1 },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <Button
+              style={{ marginTop: 75 }}
+              variant="contained"
+              endIcon={<SendIcon />}
+              onClick={() => sendLoginInfo()}
+            >
+              Send
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
